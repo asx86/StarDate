3fd55093cd17d2badc8852d7bf76d9542676a19d
Adam Stodulski <asx86@outlook.com>
2017-10-22 02:40:42 +0200

    first commit

---
+++ StarDate/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using StarDate.Models.Identity;
+
+namespace StarDate.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
--- StarDate/Startup.cs
+++ StarDate/Startup.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using StarDate.Models;
+using StarDate.Models.Identity;
 
 namespace StarDate
 {
@@ -16,6 +20,11 @@ namespace StarDate
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Stardate"));
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +37,12 @@ namespace StarDate
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseIdentity();
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hey you are not supposed to be here!");
             });
         }
     }
